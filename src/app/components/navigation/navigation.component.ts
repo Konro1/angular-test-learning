3790d89e531bb3c6e263e65bb572af4f0d3fe150
maksym,plotnikov <maksym.plotnikov@lemberg.co.uk>
2018-11-08 14:31:21 +0200

    Remove unneded

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -4,7 +4,6 @@ import {Router} from '@angular/router';
 @Component({
     selector: 'app-navigation',
     templateUrl: './navigation.component.html',
-    styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
     links = [
